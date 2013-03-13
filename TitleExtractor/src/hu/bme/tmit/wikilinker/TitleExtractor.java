e82ae3a9aa944dae4af8a4f643598d7cbe599e27
gulyasm <mgulyas86@gmail.com>
2013-03-13 07:03:29 +0100

    Parsolja a Wikipedia oldakat.

--- TitleExtractor/src/hu/bme/tmit/wikilinker/TitleExtractor.java
+++ TitleExtractor/src/hu/bme/tmit/wikilinker/TitleExtractor.java
@@ -1,8 +1,38 @@
 package hu.bme.tmit.wikilinker;
 
-public class TitleExtractor {
+import com.google.common.base.Joiner;
 
+import edu.jhu.nlp.wikipedia.PageCallbackHandler;
+import edu.jhu.nlp.wikipedia.WikiPage;
+import edu.jhu.nlp.wikipedia.WikiXMLParser;
+import edu.jhu.nlp.wikipedia.WikiXMLParserFactory;
+
+public class TitleExtractor {
+	
+	private static final String PATH = "e:\\TEMP\\enwiki-latest-pages-articles1.xml-p000000010p000010000";
+	
 	public static void main(String[] args) {
+		TitleExtractor extractor = new TitleExtractor();
+		extractor.extrac();
+	}
+
+	private void extrac() {
+		WikiXMLParser parser = WikiXMLParserFactory.getSAXParser(PATH);
+		try {
+			parser.setPageCallback(new PageCallbackHandler() {
+				
+				@Override
+				public void process(WikiPage arg0) {
+					System.out.println(Joiner.on(", ").join(arg0.getLinks()));
+					
+				}
+			});
+			parser.parse();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
+	
+	
 }
