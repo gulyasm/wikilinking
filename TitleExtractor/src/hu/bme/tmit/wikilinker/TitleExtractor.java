ee798e7e94a636c53f22eb8aa6054558cc453abb
gulyasm <mgulyas86@gmail.com>
2013-03-14 15:10:55 +0100

    Huge xml file added

--- TitleExtractor/src/hu/bme/tmit/wikilinker/TitleExtractor.java
+++ TitleExtractor/src/hu/bme/tmit/wikilinker/TitleExtractor.java
@@ -1,15 +1,17 @@
 package hu.bme.tmit.wikilinker;
 
-import com.google.common.base.Joiner;
+import java.util.Locale;
 
-import edu.jhu.nlp.wikipedia.PageCallbackHandler;
-import edu.jhu.nlp.wikipedia.WikiPage;
+import com.google.common.base.Stopwatch;
+
 import edu.jhu.nlp.wikipedia.WikiXMLParser;
 import edu.jhu.nlp.wikipedia.WikiXMLParserFactory;
 
 public class TitleExtractor {
 	
-	private static final String PATH = "e:\\TEMP\\enwiki-latest-pages-articles1.xml-p000000010p000010000";
+//	private static final String PATH = "e:\\TEMP\\wiki-dumps\\enwiki-latest-pages-articles1.xml-p000000010p000010000";
+//	private static final String PATH = "e:\\TEMP\\wiki-dumps\\enwiki-20130304-pages-meta-current12.xml-p001825001p002425000";
+	private static final String PATH = "e:\\TEMP\\wiki-dumps\\enwiki-20130304-pages-meta-current23.xml";
 	
 	public static void main(String[] args) {
 		TitleExtractor extractor = new TitleExtractor();
@@ -19,19 +21,19 @@ public class TitleExtractor {
 	private void extrac() {
 		WikiXMLParser parser = WikiXMLParserFactory.getSAXParser(PATH);
 		try {
-			parser.setPageCallback(new PageCallbackHandler() {
-				
-				@Override
-				public void process(WikiPage arg0) {
-					System.out.println(Joiner.on(", ").join(arg0.getLinks()));
-					
-				}
-			});
+			StatisticalCallback callback = new StatisticalCallback();
+			callback.setLogPagesInterval(100000);
+			parser.setPageCallback(callback);
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.start();
 			parser.parse();
+			stopwatch.stop();
+			Locale.setDefault(Locale.US);
+			System.out.println("Pages ---> \n" + callback.getParsedPages());
+			System.out.println("Time ---> \n" + stopwatch.toString());
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
 	
 	
