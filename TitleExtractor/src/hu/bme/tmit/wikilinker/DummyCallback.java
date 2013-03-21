6f9ab788053b5e4f31b29e46ae0172aeab9b37e0
gulyasm <mgulyas86@gmail.com>
2013-03-21 15:53:52 +0100

    DummyCallback a mérésekhez

---
+++ TitleExtractor/src/hu/bme/tmit/wikilinker/DummyCallback.java
@@ -0,0 +1,11 @@
+package hu.bme.tmit.wikilinker;
+
+import edu.jhu.nlp.wikipedia.WikiPage;
+
+public class DummyCallback extends AbstractPageCallback {
+
+	@Override
+	public void process(WikiPage page) {
+		super.process(page);
+	}
+}
