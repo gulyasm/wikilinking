c7cf4b556c00d37a3d90e7b1bb5c72e57b5dfa48
gulyasm <mgulyas86@gmail.com>
2013-04-06 17:39:57 +0200

    sqlite files added.

--- DatabaseTest/src/hu/bme/tmit/dbtest/DatabaseTest.java
+++ DatabaseTest/src/hu/bme/tmit/dbtest/DatabaseTest.java
@@ -1,9 +1,11 @@
 package hu.bme.tmit.dbtest;
 
 import java.io.File;
-import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 import com.almworks.sqlite4java.SQLiteException;
 import com.almworks.sqlite4java.SQLiteStatement;
@@ -13,10 +15,12 @@ import com.google.common.collect.Maps;
 public class DatabaseTest {
 
 	private static final String DB_PATH = "db/test";
-	private static final int RUNS = 1_000_000;
+	private static final int RUNS = 20_000_000;
 	private static final String TABLE_NAME = "Persons";
 	private static final String COLUMN_NAME = "Name";
 	private static Random random = new Random();
+	private static Set<Integer> randomIDs = new HashSet<>();
+	private static Set<String> randomStrings = new HashSet<>();
 
 	public static void main(String[] args) throws SQLiteException {
 		System.out.println("Test: Inserting " + RUNS + " string");
@@ -31,14 +35,18 @@ public class DatabaseTest {
 		sqlite.createTable(TABLE_NAME, COLUMN_NAME + " TEXT");
 		logtime("CREATE TABLE", stopwatch);
 		stopwatch.reset();
-		int selected = random.nextInt(RUNS);
-		String selectedString = null;
+		int index = 0;
+		while (index++ < 100_000) {
+			randomIDs.add(random.nextInt(RUNS));
+		}
+		index = 0;
+
 		stopwatch.start();
 		sqlite.beginTransaction();
 		for (int i = 0; i < RUNS; i++) {
-			String string = new BigInteger(130, random).toString(32);
-			if (i == selected) {
-				selectedString = string;
+			if (randomIDs.contains(i)) {
+				randomStrings.add(string);
+				System.out.println(string);
 			}
 			HashMap<String, String> values = Maps.newHashMap();
 			values.put(COLUMN_NAME, string);
@@ -56,14 +64,16 @@ public class DatabaseTest {
 		stopwatch.reset();
 		stopwatch.start();
 
-		SQLiteStatement stmt = sqlite.createStatement("SELECT * FROM Persons WHERE Name ='" + selectedString + "'");
-		while (stmt.step()) {
-			System.out.println("Selected row: " + stmt.columnString(0));
+		for (String s : randomStrings) {
+			SQLiteStatement stmt = sqlite.createStatement("SELECT * FROM Persons WHERE Name ='" + s + "'");
+			while (stmt.step()) {
+				System.out.println("Selected row: " + stmt.columnString(0));
+			}
+			stmt.dispose();
 		}
-		logtime("SELECT RANDOM ROW FROM " + RUNS + " ROW ", stopwatch);
+		logtime("SELECT RANDOM 100_000 ROW FROM " + RUNS + " ROW ", stopwatch);
 		stopwatch.reset();
 
-		stmt.dispose();
 		sqlite.dispose();
 
 		System.out.println("----------------------------------------");
---
+++ TitleExtractor/src/hu/bme/tmit/wikilinker/db/SQLite.java
@@ -0,0 +1,67 @@
+package hu.bme.tmit.wikilinker.db;
+
+import java.io.File;
+import java.text.MessageFormat;
+import java.util.Map;
+
+import com.almworks.sqlite4java.SQLiteConnection;
+import com.almworks.sqlite4java.SQLiteException;
+import com.almworks.sqlite4java.SQLiteStatement;
+import com.google.common.base.Joiner;
+
+public class SQLite {
+
+	private SQLiteConnection conn;
+
+	public SQLite(String path) throws SQLiteException {
+		conn = new SQLiteConnection(new File(path));
+		conn.open(true);
+	}
+
+	public void beginTransaction() throws SQLiteException {
+		conn.exec("BEGIN TRANSACTION");
+	}
+
+	public SQLiteStatement createStatement(String query) throws SQLiteException {
+		return conn.prepare(query);
+	}
+
+	public void exec(String query) throws SQLiteException {
+		conn.exec(query);
+	}
+
+	public void commitTransaction() throws SQLiteException {
+		conn.exec("COMMIT TRANSACTION");
+	}
+
+	public void delete(String table) throws SQLiteException {
+		conn.exec("DELETE FROM " + table);
+	}
+
+	public void insert(String table, Map<String, String> values) throws SQLiteException {
+		String statement = MessageFormat.format(
+				"INSERT OR REPLACE INTO {0}({1}) VALUES(''{2}'')",
+				table,
+				Joiner.on(", ").join(values.keySet()),
+				Joiner.on(", ").join(values.values()));
+		conn.exec(statement);
+	}
+
+	public void createTable(String name, String... columns) throws SQLiteException {
+		String statement = MessageFormat.format(
+				"CREATE TABLE IF NOT EXISTS {0} ({1})",
+				name,
+				Joiner.on(", ").join(columns));
+		conn.exec(statement);
+	}
+
+	public void createIndex(String table, String column, String indexName) throws SQLiteException {
+		conn.exec(MessageFormat.format("CREATE INDEX IF NOT EXISTS {0} ON {1}({2})", indexName, table, column));
+	}
+
+	public void dispose() {
+		if (conn != null) {
+			conn.dispose();
+		}
+	}
+}
